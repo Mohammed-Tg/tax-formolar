852e3621a3b75034007b1ff0703546389715eca5
Mohammed-Tg <117012421+Mohammed-Tg@users.noreply.github.com>
2026-01-04 16:47:29 +0100

    Add Excel export and session flow to blueprints

--- app/forms/routes.py
+++ app/forms/routes.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, send_file
+from flask import Blueprint, render_template, request, redirect, url_for, flash, send_file, session, current_app
 from flask_login import login_required, current_user
 from fpdf import FPDF
-import os
 
-from ..extensions import db
+from ..extensions import db, mail
 from ..models.form import Form
+from excel_export import create_multi_sheet_excel, send_form_data_email
 
 # Blueprint erstellen
 forms_bp = Blueprint('forms', __name__)
@@ -53,6 +53,18 @@ def create_form():
         db.session.add(form)
         db.session.commit()
 
+        form_data = session.get('form_data', {})
+        form_data['stammdaten'] = {
+            'name': name,
+            'surname': surname,
+            'tax_id': tax_id,
+            'tax_class': tax_class,
+            'family_status': family_status,
+            'has_children': has_children,
+            'num_children': num_children,
+        }
+        session['form_data'] = form_data
+
         # PDF dem Benutzer anbieten
         return send_file(pdf_output, as_attachment=True)
 
@@ -62,23 +74,54 @@ def create_form():
 @login_required
 def einnahmen():
     if request.method == 'POST':
-        # Verarbeiten Sie die Daten aus dem Formular
+        form_data = session.get('form_data', {})
+        form_data['einnahmen'] = request.form.to_dict()
+        session['form_data'] = form_data
         flash('Daten erfolgreich gespeichert!')
-        return render_template('forms/einnahmen.html')  # Rendern der nächsten Seite
+        return redirect(url_for('forms.ausgaben'))
     return render_template('forms/einnahmen.html')  # Rendern der nächsten Seite
 
 @forms_bp.route('/ausgaben', methods=['GET', 'POST'])
 @login_required
 def ausgaben():
-    if request.method == 'POST':
-        # Verarbeiten Sie die Daten aus dem Formular
-        flash('Daten erfolgreich gespeichert!')
-        return redirect(url_for('main.dashboard'))  # Weiterleitung zum Dashboard
     return render_template('forms/ausgaben.html')
 
 @forms_bp.route('/submit_form', methods=['POST'])
 @login_required
 def submit_form():
-    # Verarbeiten Sie die Daten aus dem Formular
+    form_data = session.get('form_data', {})
+    form_data['ausgaben'] = request.form.to_dict()
+    session['form_data'] = form_data
+
+    user_info = {
+        'first_name': current_user.first_name,
+        'last_name': current_user.last_name,
+        'email': current_user.email,
+    }
+    admin_email = current_app.config['MAIL_USERNAME']
+    send_form_data_email(form_data, user_info, mail, admin_email)
+    session.pop('form_data', None)
+
     flash('Formular erfolgreich abgeschickt!', 'success')
     return redirect(url_for('main.dashboard'))  # Weiterleitung zum Dashboard
+
+@forms_bp.route('/export_excel', methods=['POST'])
+@login_required
+def export_excel():
+    form_data = session.get('form_data', {})
+    current_form_data = request.form.to_dict()
+    if current_form_data:
+        form_data['ausgaben'] = current_form_data
+        session['form_data'] = form_data
+    if not form_data:
+        flash('Keine Formulardaten für den Excel-Export gefunden.', 'error')
+        return redirect(url_for('main.dashboard'))
+
+    excel_file = create_multi_sheet_excel(form_data)
+    filename = f"formular_{current_user.first_name}_{current_user.last_name}.xlsx"
+    return send_file(
+        excel_file,
+        as_attachment=True,
+        download_name=filename,
+        mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
