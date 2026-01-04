212ccabb0439137c03671713317448606fc3ef80
Mohammed-Tg <117012421+Mohammed-Tg@users.noreply.github.com>
2026-01-04 16:33:50 +0100

    Add optional dev user bootstrap via config

--- app/__init__.py
+++ app/__init__.py
@@ -18,11 +18,29 @@ def create_app(config_class=Config):
     from .auth.routes import auth_bp
     from .main.routes import main_bp
     from .forms.routes import forms_bp
+    from .models.user import User
     
     # Registrierung der Blueprints ohne Präfixe
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(forms_bp)
+
+    if app.config['DEV_USER_ENABLED']:
+        with app.app_context():
+            existing_user = User.query.filter_by(username=app.config['DEV_USERNAME']).first()
+            if existing_user is None:
+                dev_user = User(
+                    first_name=app.config['DEV_FIRST_NAME'],
+                    last_name=app.config['DEV_LAST_NAME'],
+                    username=app.config['DEV_USERNAME'],
+                    email=app.config['DEV_EMAIL'],
+                    password='',
+                    verification_code='DEV000',
+                    is_verified=True,
+                )
+                dev_user.set_password(app.config['DEV_PASSWORD'])
+                db.session.add(dev_user)
+                db.session.commit()
     
     # Direkte Testroute
     @app.route('/test')
